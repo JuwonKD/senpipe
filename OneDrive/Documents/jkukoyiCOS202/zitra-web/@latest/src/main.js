0f635ad3e6f7706e01b16486d7bbb29c4f9e2ba5
JuwonKD <juwonkukoyi9@gmail.com>
2026-08-06 13:29:11 +0100

    login and landing done

--- OneDrive/Documents/jkukoyiCOS202/zitra-web/@latest/src/main.js
+++ OneDrive/Documents/jkukoyiCOS202/zitra-web/@latest/src/main.js
@@ -1,3 +1,37 @@
 import './style.css'
 
 
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", () => {window.location.href = "/login.html";});
+}
+
+const otpInputs = document.querySelectorAll("input[maxlength='1']");
+
+otpInputs.forEach((input, index) => {
+
+    input.addEventListener("input", () => {
+
+        // Allow only numbers
+        input.value = input.value.replace(/\D/g, "");
+
+        // Move to next box
+        if (input.value && index < otpInputs.length - 1) {
+            otpInputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+
+        // Move back when Backspace is pressed
+        if (
+            e.key === "Backspace" &&
+            input.value === "" &&
+            index > 0
+        ) {
+            otpInputs[index - 1].focus();
+        }
+    });
+
+});
